3d11c3ca9b555ca82e8d58b5d7aa24a8e59911c9
Koketso Neo Masasanya <101709908+ST10225793@users.noreply.github.com>
2024-11-03 03:35:42 +0200

    Update ProfileViewModel.cs

--- Gift Of The Givers Web App/Models/ProfileViewModel.cs
+++ Gift Of The Givers Web App/Models/ProfileViewModel.cs
@@ -1,14 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace Gift_Of_The_Givers_Web_App.Models
 {
     public class ProfileViewModel
     {
         [Required]
-        public string Username { get; set; }
+        public string Username { get; set; } = string.Empty;
 
         [Required]
         [EmailAddress]
-        public string Email { get; set; }
-    }
+        public string Email { get; set; } = string.Empty
+    } 
 }
