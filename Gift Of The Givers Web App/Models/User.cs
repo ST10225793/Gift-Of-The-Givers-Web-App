4ac31f5fa6089befb8a1aecf2da86fe8c667a6d1
Koketso Neo Masasanya <101709908+ST10225793@users.noreply.github.com>
2024-11-03 03:19:50 +0200

    Update User.cs

--- Gift Of The Givers Web App/Models/User.cs
+++ Gift Of The Givers Web App/Models/User.cs
@@ -9,19 +9,19 @@ namespace Gift_Of_The_Givers_Web_App.Models
 
         [Required(ErrorMessage = "Username is required.")]
         [StringLength(50, ErrorMessage = "Username must be less than 50 characters.")]
-        public required string Username { get; set; }
+        public string Username { get; set; }
 
         [Required(ErrorMessage = "Please confirm your password.")]
         [DataType(DataType.Password)]
-        public required string ConfirmPassword { get; set; }
+        public string ConfirmPassword { get; set; }
 
         [Required(ErrorMessage = "Please select a role.")]
-        public required string Role { get; set; }
+        public string Role { get; set; }
 
         // This property will be used for the raw password input during registration
         [Required(ErrorMessage = "Password is required.")]
         [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters.")]
         [DataType(DataType.Password)]
-        public required string Password { get; set; }
+        public string Password { get; set; }
     }
 }
