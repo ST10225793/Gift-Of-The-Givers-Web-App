3fd5076df9b44af02bd8313fafb025eebfd9745d
Koketso Neo Masasanya <101709908+ST10225793@users.noreply.github.com>
2024-11-03 02:46:27 +0200

    Update RegisterViewModel.cs

--- Gift Of The Givers Web App/Models/RegisterViewModel.cs
+++ Gift Of The Givers Web App/Models/RegisterViewModel.cs
@@ -6,24 +6,24 @@ namespace Gift_Of_The_Givers_Web_App.Models
     {
         [Required]
         [Display(Name = "Username")]
-        public string Username { get; set; }
+        public required string Username { get; set; }
 
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
-        public string Email { get; set; }
+        public required string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
-        public string Password { get; set; }
+        public required string Password { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         [Display(Name = "Confirm Password")]
-        public string ConfirmPassword { get; set; }
+        public required string ConfirmPassword { get; set; }
 
-        public string Role {  get; set; }
+        public required string Role {  get; set; }
     }
 }
